ad36a0b224250c987966d3a442ff324dc197b0f9
Brant Wellman <brantwellman@gmail.com>
2015-01-21 02:14:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieRanker::Application.config.secret_key_base = 'b008164e09bca474f70705af4e3b571dcfa5da2b4efa46149a88ff8ff3b4182333fdd46fae8961952767dadf6bddd01b5fdf524cb5f8bb705f9d4380f58353ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovieRanker::Application.config.session_store :cookie_store, key: '_movie-ranker_session'
