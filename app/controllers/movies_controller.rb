388d79c92227c7b5580c14fbaa0ac9bc3ab26eb8
Brant Wellman <brantwellman@gmail.com>
2015-01-28 23:19:03 +0000

    Added submit form and create function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,19 @@ class MoviesController < ApplicationController
 	end
 
 	def new
+		@movie = Movie.new
+	end
+
+	def create
+		Movie.create(movie_params)
+		redirect_to root_path
+	end
+
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description)
 	end
 
 end
