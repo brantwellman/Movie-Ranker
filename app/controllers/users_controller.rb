e489d34b0d051f07220143023bbefcdbbb152d53
Brant Wellman <brantwellman@gmail.com>
2015-01-31 00:53:06 +0000

    Started writing group_by code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
 	def show
-		@user = User.find(params[:id])
-		@movies = @user.movies
+		@movies = User.find(params[:id]).movies
+		@movies_year = @movies.group_by { |movie| movie.year }
 		
 	end
 
