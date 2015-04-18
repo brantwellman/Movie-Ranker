e07f2e740d15375443cd7944ba5965b3900986ad
Brant Wellman <brantwellman@gmail.com>
2015-04-18 00:24:24 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+	username: 'Brant',
+	email: 'myemail@email.com',
+	password: 'validpass',
+	password_confirmation: 'validpass'
+)
+
+6.times do |i|
+	List.create(year: (2015 - i), user_id: 1)
+end
+
+10.times do |i|
+	Movie.create(title: "The Red Dog ##{i}", user_id: 1, list_id: 1)
+end
+
+11.times do |i|
+	Movie.create(title: "The Purple Cape ##{i}", user_id: 1, list_id: 2)
+end
+
+12.times do |i|
+	Movie.create(title: "Bill: The Mastermind ##{i}", user_id: 1, list_id: 3)
+end
+
+13.times do |i|
+	Movie.create(title: "Queen Perth ##{i}", user_id: 1, list_id: 4)
+end
+
+14.times do |i|
+	Movie.create(title: "The Collar Controversy ##{i}", user_id: 1, list_id: 5)
+end
+
+
